6c27b58b8263160d0c7282f87f8bbde914ce5ce3
lobo83 <inmortalland83@gmail.com>
2021-10-19 08:22:12 +0200

    añadido ejemplo tonto de multiprovider con Inject y useValue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Inject, InjectionToken } from '@angular/core';
 import { Router } from '@angular/router';
+
+export const TOKEN = new InjectionToken<string>('MyToken');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [{provide: TOKEN, useValue: 'valor1',multi:true},
+  {provide: TOKEN, useValue: 'valor2',multi:true}]
 })
 export class AppComponent {
   title = 'ngAuction';
 
-  constructor(private _router: Router){}
+  constructor(private _router: Router, @Inject(TOKEN) myInjectedToken: string){
+    console.log("holaaaa "+myInjectedToken);
+  }
   navigateToProducDetail(){
     this._router.navigate(["/product"],{skipLocationChange: true});
   }
